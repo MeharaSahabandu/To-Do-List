6e88e63b8c448d44b7fc389b70122ba74368bfc5
Mehara Sahabandu <119718801+MeharaSahabandu@users.noreply.github.com>
2023-02-05 21:32:06 +0530

    database connected

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -11,6 +11,9 @@ const PORT = process.env.PORT || 8070;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+
+//connect database
 const URL = process.env.MONGODB_URL;
 
 mongoose.connect(URL, ()=>{
@@ -27,9 +30,7 @@ connection.once('open', ()=>{
 })
 
 
-
 app.listen(PORT,()=>{
     console.log(`Server is up and running on port number: ${PORT} !`);
 })
-
 
